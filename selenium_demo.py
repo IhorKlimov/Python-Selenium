2f64ae0af554f838088ce9e1f32fd612a6fa11f0
IhorKlimov <iklimov6@gmail.com>
2017-03-20 07:30:40 +0100

    added files

---
+++ selenium_demo.py
@@ -0,0 +1,27 @@
+from array import array
+import os.path
+import numpy as np
+import sys
+from selenium import webdriver
+import click as click
+import clipboard
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def login():
+    email = browser.find_element_by_id('email')
+    password = browser.find_element_by_id('pass')
+    email.send_keys('your_email')
+    password.send_keys('your_password')
+    browser.find_element_by_id('u_0_s').submit()
+
+if __name__ == '__main__':
+    # browser = webdriver.Firefox(executable_path='./geckodriver')
+    browser = webdriver.Chrome(executable_path='./chromedriver')
+    browser.get('https://facebook.com')
+    login()
+    
+    
